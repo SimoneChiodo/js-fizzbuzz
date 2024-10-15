a63ab8d5bf37829ad48c8958ab859e417f6f574e
SimoneChiodo <simochi.005@gmail.com>
2024-10-15 14:37:27 +0200

    add logic

--- js/main.js
+++ js/main.js
@@ -6,12 +6,27 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 */
 
 // Ciclo da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    // Creo una variabile per l'output (si resetta ad ogni ciclo)
+    let output = "";
 
-// SE l'indice è divisibile per 3 e 5
-// ALLORA output: FizzBuzz
+    // SE l'indice è divisibile per 3 e 5
+    if (i % 3 === 0 && i % 5 === 0) {
+        // ALLORA output: FizzBuzz
+        output += `Number: ${i}; FizzBuzz`;
+    }
+    // ALTRIMENTI SE l'indice è divisibile per 3
+    else if (i % 3 === 0) {
+        // ALLORA output: Fizz
+        output += `Number: ${i}; Fizz`;
+    }
+    // ALTRIMENTI SE l'indice è divisibile per 5
+    else if (i % 5 === 0) {
+        // ALLORA output: Buzz
+        output += `Number: ${i}; Buzz`;
+    } else {
+        output += `Number: ${i}`;
+    }
 
-// ALTRIMENTI SE l'indice è divisibile per 3
-// ALLORA output: Fizz
-
-// ALTRIMENTI SE l'indice è divisibile per 5
-// ALLORA output: Buzz
+    console.log(output);
+}
