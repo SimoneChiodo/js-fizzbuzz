0abc1a62599174bdbc2f12492192071adce115a0
SimoneChiodo <simochi.005@gmail.com>
2024-10-15 14:37:05 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/*
+ * Testo esercizio:
+Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+*/
+
+// Ciclo da 1 a 100
+
+// SE l'indice è divisibile per 3 e 5
+// ALLORA output: FizzBuzz
+
+// ALTRIMENTI SE l'indice è divisibile per 3
+// ALLORA output: Fizz
+
+// ALTRIMENTI SE l'indice è divisibile per 5
+// ALLORA output: Buzz
